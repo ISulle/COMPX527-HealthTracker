55f03524ae429cd8913e4f019b310adfe505d542
Sulhan <34217144+Sulle28@users.noreply.github.com>
2023-09-01 00:06:26 +1200

    Added diabetes page with prediction

--- HealthTracker/Controllers/HomeController.cs
+++ HealthTracker/Controllers/HomeController.cs
@@ -6,9 +6,14 @@ using HealthTracker.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Http.Headers;
 using Amazon;
-using HealthTracker.Data;
-using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Microsoft.AspNetCore.Cors;
+using Newtonsoft.Json;
+using Microsoft.VisualBasic.FileIO;
 
 namespace HealthTracker.Controllers;
 
@@ -16,6 +21,7 @@ namespace HealthTracker.Controllers;
 public class HomeController : Controller
 {
     private readonly IDynamoDBContext _context;
+    private readonly IWebHostEnvironment _env;
     //public HomeController(IDynamoDBContext context)
     //{
     //    _context = context;
@@ -26,6 +32,7 @@ public class HomeController : Controller
         var credentials = new BasicAWSCredentials(config["AWS:AccessKey"], config["AWS:SecretKey"]);
         var client = new AmazonDynamoDBClient(credentials, RegionEndpoint.APSoutheast2);
         _context = new DynamoDBContext(client);
+        _env = env;
     }
 
     private string GetUsername()
@@ -44,12 +51,22 @@ public class HomeController : Controller
         return View(user);
     }
 
+    private async Task SaveUser(UserDto dto)
+    {
+        // Create user in DynamoDB
+        var user = await GetUser();
+        user.Name = dto.Name;
+        user.Gender = dto.Gender;
+        user.DOB = dto.DOB.ToString("MM/dd/yyyy");
+        user.Height = dto.Height;
+        user.Weight = dto.Weight;
+        await _context.SaveAsync(user);
+    }
+
     [HttpPost]
     public async Task<IActionResult> Index(UserDto dto)
     {
-        // Create user in DynamoDB
-        var user = new UserInformation(GetUsername(), dto.Name, dto.Gender, dto.DOB, dto.Height, dto.Weight);
-        await _context.SaveAsync(user);
+        await SaveUser(dto);
         return RedirectToAction("Index");
     }
 
@@ -65,14 +82,7 @@ public class HomeController : Controller
     [HttpPost]
     public async Task<IActionResult> Register(UserDto dto)
     {
-        var user = await GetUser();
-        user.Name = dto.Name;
-        user.Gender = dto.Gender;
-        user.DOB = dto.DOB.ToString("MM/dd/yyyy");
-        user.Height = dto.Height;
-        user.Weight = dto.Weight;
-        // Create user in DynamoDB
-        await _context.SaveAsync(user);
+        await SaveUser(dto);
         return RedirectToAction("Index");
     }
 
@@ -167,83 +177,11 @@ public class HomeController : Controller
         {
             user.Init();
             // Make prediction
-            MakePrediction(user);
-            //diabetesData.Prediction = prediction[0];
+            var prediction = await GetPrediction(user);
+            diabetesData.Prediction = prediction;
         }
         return View(diabetesData);
     }
-
-    private void MakePrediction(UserInformation user)
-    {
-        string pythonExePath = @"C:\Path\To\Python\python.exe";
-        string pythonScriptPath = @"C:\Path\To\Your\PythonScript.py";
-        // Create a process to run the Python script
-        using (Process process = new Process())
-        {
-            process.StartInfo.FileName = pythonExePath;
-            process.StartInfo.Arguments = pythonScriptPath;
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.RedirectStandardOutput = true;
-            // Start the process
-            process.Start();
-            // Read the standard output of the process
-            string output = process.StandardOutput.ReadToEnd();
-            // Wait for the process to exit
-            process.WaitForExit();
-            // Display the output from the Python script
-            Console.WriteLine("Python Script Output:");
-            Console.WriteLine(output);
-        }
-    }
-
-
-    //public async Task<bool[]> MakePredictionAsync(UserInformation user)
-    //{
-    //    // Prepare the input data
-    //    var input = new DiabetesInput
-    //    {
-    //        Age = user.Age,
-    //        Gender = user.Gender == "Male" ? 1 : 0,
-    //        Polyuria = user.MedicalHistory.Polyuria,
-    //        Polydipsia = user.MedicalHistory.Polydipsia,
-    //        SuddenWeightLoss = user.MedicalHistory.SuddenWeightLoss,
-    //        Weakness = user.MedicalHistory.Weakness,
-    //        Polyphagia = user.MedicalHistory.Polyphagia,
-    //        GenitalThrush = user.MedicalHistory.GenitalThrush,
-    //        VisualBlurring = user.MedicalHistory.VisualBlurring,
-    //        Itching = user.MedicalHistory.Itching,
-    //        Irritability = user.MedicalHistory.Irritability,
-    //        DelayedHealing = user.MedicalHistory.DelayedHealing,
-    //        PartialParesis = user.MedicalHistory.PartialParesis,
-    //        MuscleStiffness = user.MedicalHistory.MuscleStiffness,
-    //        Alopecia = user.MedicalHistory.Alopecia,
-    //        Obesity = user.MedicalHistory.Obesity,
-    //    };
-    //    // Send the POST request to the Python API
-    //    HttpResponseMessage response = await _client.PostAsJsonAsync("http://localhost:5000/predict", input);
-
-    //    // Check if the request was successful
-    //    if (response.IsSuccessStatusCode)
-    //    {
-    //        // Read the response content
-    //        using (Stream responseStream = await response.Content.ReadAsStreamAsync())
-    //        {
-    //            // Deserialize the response using a JSON deserializer
-    //            var predictionResult = await JsonSerializer.DeserializeAsync<PredictionResponse>(responseStream);
-    //            return predictionResult.Prediction;
-    //        }
-    //    }
-    //    else
-    //    {
-    //        throw new Exception($"API request failed with status code: {response.StatusCode}");
-    //    }
-    //}
-
-    //public class PredictionResponse
-    //{
-    //    public bool[] Prediction { get; set; }
-    //}
-
 
     [HttpPost]
     public async Task<IActionResult> Diabetes(DiabetesData dto)
@@ -295,5 +233,77 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public async Task<bool> GetPrediction(UserInformation user)
+    {
+        var apiUrl = "http://host.docker.internal:32000/predict";
+        using (var client = new HttpClient())
+        {
+            // Construct the input JSON data
+            var jsonData = new
+            {
+                Age = user.Age,
+                Gender = user.Gender == "Male" ? 0 : 1,
+                Polyuria = user.MedicalHistory.Polyuria,
+                Polydipsia = user.MedicalHistory.Polydipsia,
+                SuddenWeightLoss = user.MedicalHistory.SuddenWeightLoss,
+                Weakness = user.MedicalHistory.Weakness,
+                Polyphagia = user.MedicalHistory.Polyphagia,
+                GenitalThrush = user.MedicalHistory.GenitalThrush,
+                VisualBlurring = user.MedicalHistory.VisualBlurring,
+                Itching = user.MedicalHistory.Itching,
+                Irritability = user.MedicalHistory.Irritability,
+                DelayedHealing = user.MedicalHistory.DelayedHealing,
+                PartialParesis = user.MedicalHistory.PartialParesis,
+                MuscleStiffness = user.MedicalHistory.MuscleStiffness,
+                Alopecia = user.MedicalHistory.Alopecia,
+                Obesity = user.MedicalHistory.Obesity
+            };
+            var jsonPayload = JsonConvert.SerializeObject(jsonData);
+            var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await client.PostAsync(apiUrl, content);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var r = JsonConvert.DeserializeObject<PredictionResponse>(jsonResponse);
+                return r.Prediction;
+            }
+        }
+        return false;
+    }
+
+    public class PredictionResponse
+    {
+        public bool Prediction { get; set; }
+    }
+
+    [HttpGet]
+    public IActionResult GetCsvData()
+    {
+        string path = Path.Combine(_env.WebRootPath, "data", "diabetes_data.csv");
+        var resultList = new List<Dictionary<string, string>>();
+        using (var parser = new TextFieldParser(path))
+        {
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(",");
+            // Assuming the first row contains the column headers
+            string[] headers = parser.ReadFields();
+            while (!parser.EndOfData)
+            {
+                string[] fields = parser.ReadFields();
+                if (fields != null && headers != null)
+                {
+                    var dict = new Dictionary<string, string>();
+                    for (int i = 0; i < headers.Length; i++)
+                    {
+                        dict.Add(headers[i], fields[i]);
+                    }
+                    resultList.Add(dict);
+                }
+            }
+        }
+        return Json(resultList);
     }
 }
